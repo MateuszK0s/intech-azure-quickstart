b6afb547b101cb7f8abaf8dc46e37495849a5532
MateuszK0s <kosmateusz99@gmail.com>
2025-02-22 00:16:01 +0100

    test

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ resource "azurerm_resource_group" "rg" {
     location = "polandcentral"
     name = "${random_pet.prefix.id}-rg"
 }
+
+resource "random_pet" "prefix" {
+    prefix = "intech"
+    length = 1
+}
