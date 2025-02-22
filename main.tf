bd0439ae4d75e46d8ffe89a3479d33738a3a9a68
MateuszK0s <kosmateusz99@gmail.com>
2025-02-22 17:55:35 +0100

    added vnets and subnets

--- main.tf
+++ main.tf
@@ -26,6 +26,6 @@ resource "azurerm_subnet" "my_terraform_subnet1"{
 resource "azurerm_subnet" "my_terraform_subnet2" {
     name = "${random_pet.prefix.id}-subnet"
     resource_group_name = azurerm_resource_group.rg.name
-    virtual_network_name = my_terraform_network.name
+    virtual_network_name = azurerm_virtual_network.my_terraform_network.name
     address_prefixes = ["10.0.1.0/24"]
 }
