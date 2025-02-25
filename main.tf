fa34ee9af749c6cae6d3c8887c5b467dcc9e5b46
MateuszK0s <kosmateusz99@gmail.com>
2025-02-25 11:57:07 +0100

    added vms variables and ni cards

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # Resource Group
 resource "azurerm_resource_group" "rg" {
-    location = "polandcentral"
+    location = var.resource_group_location
     name = "${random_pet.prefix.id}-rg"
 }
 
 resource "random_pet" "prefix" {
-    prefix = "intech"
+    prefix = var.resource_group_name_prefix
     length = 1
 }
 
@@ -16,14 +16,14 @@ resource "azurerm_virtual_network" "my_terraform_network" {
     resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "my_terraform_subnet1"{
+resource "azurerm_subnet" "my_terraform_subnet_1"{
     name = "AzureBastionSubnet"
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.my_terraform_network.name
     address_prefixes = ["10.0.0.0/24"]
 }
 
-resource "azurerm_subnet" "my_terraform_subnet2" {
+resource "azurerm_subnet" "my_terraform_subnet_2" {
     name = "${random_pet.prefix.id}-subnet"
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.my_terraform_network.name
